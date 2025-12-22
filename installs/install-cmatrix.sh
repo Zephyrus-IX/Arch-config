382d72f0eff068b314e47273312301978df97185
Zephyrus-IX <jaedenstankevich@gmail.com>
2025-12-22 17:56:37 +0000

    Added cmatrix installer and made scripts executable

---
+++ installs/install-cmatrix.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# If dependencies are needed grab them first
+
+
+# Check if cmatrix is installed already or not
+if pacman -Qi cmatrix &> /dev/null; then
+  echo "Cmatrix is already installed"
+else
+  pacman -S --noconfirm --needed cmatrix
+fi
