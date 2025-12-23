099fce6b72c28a7fb44ff689ce1192a3c6618ab3
Zephyrus-IX <jaedenstankevich@gmail.com>
2025-12-23 00:26:41 -0600

    Added onefetch installer

---
+++ installs/install-onefetch.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# If dependencies are needed grab them first
+
+
+# Check if onefetch is installed already or not
+if pacman -Qi onefetch &> /dev/null; then
+  echo "onefetch is already installed"
+else
+  pacman -S --noconfirm --needed onefetch
+fi
