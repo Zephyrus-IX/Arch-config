23f14ae88f5ccc4a4f01e42d74e74a5d44ef3c1a
Zephyrus-IX <jaedenstankevich@gmail.com>
2025-12-23 00:48:32 -0600

    Added figlet installer

---
+++ installs/install-figlet.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# If dependencies are needed grab them first
+
+
+# Check if figlet is installed already or not
+if pacman -Qi figlet &> /dev/null; then
+  echo "figlet is already installed"
+else
+  pacman -S --noconfirm --needed figlet
+fi
