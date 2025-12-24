e68ec8b7c3c370e61519c3766b87d442bae18b9e
Zephyrus-IX <jaedenstankevich@gmail.com>
2025-12-24 11:51:04 -0600

    Added fastfetch installer

---
+++ installs/install-fastfetch.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# If dependencies are needed grab them first
+if pacman -Qi glibc &> /dev/null; then
+  echo "glibc is already installed"
+else
+  sudo pacman -S --noconfirm --needed glibc
+fi
+
+if pacman -Qi yyjson &> /dev/null; then
+  echo "yyjson is already installed"
+else
+  sudo pacman -S --noconfirm --needed yyjson
+fi
+
+
+# Check if fastfetch is installed already or not
+if pacman -Qi fastfetch &> /dev/null; then
+  echo "fastfetch is already installed"
+else
+  sudo pacman -S --noconfirm --needed fastfetch
+fi
