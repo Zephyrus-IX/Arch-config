bbc29da4833b1184096a732401c40010e4d0514a
Zephyrus-IX <jaedenstankevich@gmail.com>
2026-01-05 13:24:41 -0600

    Created stow packages for dotfiles and modified install lists and scripts

--- scripts/40-session.sh
+++ scripts/40-session.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env bash
+set -e
 
-# uwsm + hyprland session glue
+# Call stow to install dotfiles from the repository
+DOTFILES_DIR="$HOME/omarchy-config/dotfiles"   # adjust to your repo location
+
+if [ -d "$DOTFILES_DIR" ]; then
+  cd "$DOTFILES_DIR"
+  stow -t "$HOME" hypr swww
+fi
