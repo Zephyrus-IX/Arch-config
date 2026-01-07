3f44ec4bdea871ba88618e5758da0dcbc5c5898a
Zephyrus-IX <jaedenstankevich@gmail.com>
2026-01-06 20:31:00 -0600

    Added global service enables for audio stack

--- scripts/30-enable-services.sh
+++ scripts/30-enable-services.sh
@@ -9,3 +9,13 @@ sudo systemctl enable --now NetworkManager.service
 sudo systemctl enable --now sddm.service
 
 echo "Services enabled."
+
+
+# Enable audio services
+echo "Enabling Pipewire (global user services)..."
+
+sudo systemctl --global enable pipewire.service
+sudo systemctl --global enable pipewire-pulse.service
+sudo systemctl --global enable wireplumber.service
+
+echo "Pipewire global user services enabled."
