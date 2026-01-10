4dee8e634f7eb180914ddd6e15e7877e8255624f
Zephyrus-IX <jaedenstankevich@gmail.com>
2026-01-09 21:56:29 -0600

    A lot of restructuring, new layouts, better scripts

--- dotfiles/rofi/.config/rofi/scripts/launcher.sh
+++ dotfiles/rofi/.config/rofi/scripts/launcher.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -euo pipefail
-exec rofi -show drun -theme "$HOME/.config/rofi/layouts/launchers/type-1/style-1/style-1.rasi"
+exec rofi -show drun -theme "$HOME/.config/rofi/layouts/launchers/type-2/style-2/style-2.rasi"
